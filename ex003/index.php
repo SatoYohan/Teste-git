71e6065bce8e388c7bbf2efbe08ecc3d45e805b4
SatoYohan <satoabner.as@gmail.com>
2023-11-07 11:31:58 -0300

    feat:ex003 curso em video

---
+++ ex003/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em php</title>
+</head>
+<body>
+    <h1>Tipos primitivos em php</h1>
+    <?php 
+    //0x= hexadecimal 0b=binário 0=octal
+
+    //  $num=0x14;
+    //  print "O valor da variavel é: $num";
+
+    //  $v=true;
+    //  var_dump($v); //mostra o tipo primitivo em tela
+        
+    //  $v="Gustavo";
+    //  var_$var="400";dump($v); //aparece na tela string(7)"Gustavo"
+        
+    //  $num=3e4; //3.(10)elevado a 4 = 30000
+    //  print "o valor é: $num"; 
+    //  var_dump($num); //sem forçar potência aparece como float
+
+    //  $var=(integer)"2.8"; //forçei a string de 3 digitos a ser um inteiro
+    //  var_dump($var);
+
+    //  $casado = false;
+    //  var_dump($casado);
+    //  print "Casado é: $casado"; //no print e echo, bool false não aparece e true mostra 1
+
+    //  $vet=[6, 2.5, "maria", 3, false];
+    //  var_dump($vet); //posso misturar os tipos em um só vetor!!!!!
+
+   /*
+   *class Pessoa{
+   *     private string $nome;
+   * }
+
+   * $p = new Pessoa;
+   * var_dump($p);
+    */ 
+
+    
+    ?>
+</body>
+</html>
